f93cee741deed1cc93d448286b3ca51aafbe971e
Jaynard Lupo <jaynardmontevilla.lupo25@bicol-u.edu.ph>
2026-08-30 15:35:49 +0800

    Update CustomerList title and version label with my initials

--- BillingSystem/CustomerListForm.Designer.cs
+++ BillingSystem/CustomerListForm.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
-            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
             lblTitle = new Label();
             dgvCustomers = new DataGridView();
             CustomerID = new DataGridViewTextBoxColumn();
@@ -74,8 +74,8 @@
             // 
             // CustomerID
             // 
-            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            CustomerID.DefaultCellStyle = dataGridViewCellStyle3;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            CustomerID.DefaultCellStyle = dataGridViewCellStyle1;
             CustomerID.HeaderText = "ID";
             CustomerID.MinimumWidth = 6;
             CustomerID.Name = "CustomerID";
@@ -83,8 +83,8 @@
             // 
             // FullName
             // 
-            dataGridViewCellStyle4.Alignment = DataGridViewContentAlignment.MiddleCenter;
-            FullName.DefaultCellStyle = dataGridViewCellStyle4;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            FullName.DefaultCellStyle = dataGridViewCellStyle2;
             FullName.HeaderText = "FullName";
             FullName.MinimumWidth = 6;
             FullName.Name = "FullName";
@@ -178,7 +178,7 @@
             Controls.Add(lblTitle);
             Name = "CustomerListForm";
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "Billing System v1.0 – Login (J.L.) ";
+            Text = "Billing System v1.0 – CustomerList (J.L.) ";
             Load += CustomerListForm_Load;
             ((System.ComponentModel.ISupportInitialize)dgvCustomers).EndInit();
             ResumeLayout(false);
